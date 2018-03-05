63e64d8cc26c14e3e6f21128f392ffb1fd7dcca7
Challen <jianglun465@sina.com>
2018-03-06 04:33:56 +0800

    最终修补，等待发布

--- routes/user.js
+++ routes/user.js
@@ -4,10 +4,10 @@ const user = require('../controllers/user')
 const route = express.Router()
 
 // 账户
-route.post('/login', user.login.bind(user))
-route.post('/register', user.register.bind(user))
-route.put('/user', user.updateUserInfo.bind(user))
-route.post('/getValidCode', user.getValidCode.bind(user))
-route.put('/password', user.changePassword.bind(user))
+route.post('/api/login', user.login.bind(user))
+route.post('/api/register', user.register.bind(user))
+// route.put('/api/user', user.updateUserInfo.bind(user))
+// route.post('/api/getValidCode', user.getValidCode.bind(user))
+// route.put('/api/password', user.changePassword.bind(user))
 
 module.exports = route
